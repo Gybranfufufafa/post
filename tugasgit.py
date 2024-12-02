8a22598fdcea8e260ba553bbc6776baa1c0da21d
Gybran <Abdullahgybran0107@gmail.com>
2024-12-02 14:30:31 +0700

    versi 4

---
+++ tugasgit.py
@@ -0,0 +1,11 @@
+data_panen ={
+    'lokasi1' : {
+        'nama_lokasi' : 'Kebun A',
+        'hasil_panen': {
+            'padi':1200,
+            'jagung': 800,
+            'kedelai':500
+        }
+    },
+    
+}
